1b0afc21d888572a2f8bde6fc0275f0d6cb6e504
wiwi1806 <lucile.gombert.lg@gmail.com>
2021-08-05 18:48:37 +0200

    portfolio V2

---
+++ contact.js
@@ -0,0 +1,129 @@
+// Contact form
+
+function checkInputs() {
+  let btnSubmit = document.getElementById("btn_submit");
+
+  btnSubmit.addEventListener('click',function(e) {
+    let name = document.getElementById('name');
+    let nameMissing = document.getElementById('nameMissing');
+    let nameValidation = /^[a-zA-ZéèêàçîïÉÈÎÏ]{2,}([-'\s][a-zA-ZéèêàçîïÉÈÎÏ]{2,})?/;
+
+    if (name.validity.valueMissing) {
+      e.preventDefault();
+
+      nameMissing.textContent = 'Ce champ est obligatoire';
+      nameMissing.style.color = 'red';
+
+      setTimeout(function() {
+        nameMissing.textContent = '';
+      },5000);
+
+    } else if (nameValidation.test(name.value) == false) {
+      e.preventDefault();
+
+      nameMissing.textContent = 'Format de saisie invalide';
+      nameMissing.style.color = 'orange';
+
+      setTimeout(function() {
+        nameMissing.textContent = '';
+      },5000);
+
+    } else {
+      nameMissing.textContent = '';
+    }
+
+    
+    let email = document.getElementById('email');
+    let emailMissing = document.getElementById('emailMissing');
+    let emailValidation = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}/;
+
+    if (email.validity.valueMissing) {
+      e.preventDefault();
+
+      emailMissing.textContent = 'Ce champ est obligatoire';
+      emailMissing.style.color = 'red';
+
+      setTimeout(function() {
+        emailMissing.textContent = '';
+      },5000);
+
+    } else if (emailValidation.test(email.value) == false) {
+      e.preventDefault();
+
+      emailMissing.textContent = 'Format de saisie invalide';
+      emailMissing.style.color = 'orange';
+
+      setTimeout(function() {
+        emailMissing.textContent = '';
+      },5000);
+
+    } else {
+      emailMissing.textContent = '';
+    }
+
+    let message = document.getElementById('message');
+    let messageMissing = document.getElementById('messageMissing');
+    let messageValidation = /^[^><%#!$/(){}]{15,400}$/;
+
+    if (message.validity.valueMissing) {
+      e.preventDefault();
+
+      messageMissing.textContent = 'Ce champ est obligatoire';
+      messageMissing.style.color = 'red';
+
+      setTimeout(function() {
+        messageMissing.textContent = '';
+      },5000);
+
+    } else if (messageValidation.test(message.value) == false) {
+      e.preventDefault();
+
+      messageMissing.textContent = 'Votre message doit contenir entre 15 et 400 caractères sans caractères spéciaux';
+      messageMissing.style.color = 'orange';
+
+      setTimeout(function() {
+        messageMissing.textContent = '';
+      },5000);
+
+    } else {
+      messageMissing.textContent = '';
+    }
+  });
+}
+
+
+function validateMessage() {
+  let name = document.getElementById("name");
+  let email = document.getElementById("email");
+  let message = document.getElementById("message"); 
+  let formContent = document.getElementById("form_content");
+  let formResponse = document.querySelector(".form_response");
+
+  formContent.addEventListener("submit", function (e) {
+    e.preventDefault();
+    
+    sendEmail(name.value, email.value, message.value);
+
+    formResponse.innerHTML = "Votre message a bien été envoyé à Lucile !";
+    
+    setTimeout(function() {
+      formResponse.innerHTML = "";
+    },8000);
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value= "";
+    document.getElementById("message").value= "";
+  });
+}
+
+
+function sendEmail(name, email, message) { 
+  emailjs.send('service_5r0twke', 'template_x9brcpp', {
+    from_name: name,
+    message: message
+  });
+}
+
+
+checkInputs();
+validateMessage();
---
+++ index.js
@@ -0,0 +1,185 @@
+// Navbar responsive
+let navToggle = document.querySelector('.nav_toggle');
+let nav = document.querySelector('.nav_bloc');
+
+navToggle.addEventListener('click', function() {
+  navToggle.classList.toggle('active');
+  nav.classList.toggle('active')
+})
+
+
+
+// Canvas
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const partciclesArray = [];
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+const mouse = {
+  x: undefined,
+  y: undefined,
+}
+
+canvas.addEventListener('click', function(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  for (let i = 0; i < 10; i++) {
+    partciclesArray.push(new Particle());
+  }
+})
+ 
+
+canvas.addEventListener('mousemove', function(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  for (let i = 0; i < 5; i++) {
+    partciclesArray.push(new Particle());
+  }
+})
+
+function drawCircle() {
+  ctx.fillStyle = '#2fa09c';
+  ctx.beginPath();
+  ctx.arc(mouse.x, mouse.y, 5, 0, Math.PI * 2);
+  ctx.fill();
+}
+const settings = {
+  colors: ['#e3f5f4', '#92d5d0', '#66ccc4', '#38bcb3', '#07a298'],
+};
+
+class Particle {
+  constructor() {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+    this.color = settings.colors[Math.round(Math.random() * settings.colors.length)]; 
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if(this.size >= 0.32) this.size -= 0.1;
+  }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath(); 
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function handleParticles() {
+  for(let i = 0; i < partciclesArray.length; i++) {
+    partciclesArray[i].update();
+    partciclesArray[i].draw();
+    for(let j = i; j < partciclesArray.length; j++) {
+      const dx = partciclesArray[i].x - partciclesArray[j].x;
+      const dy = partciclesArray[i].y - partciclesArray[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+      if(distance < 100) {
+        ctx.beginPath();
+        ctx.strokeStyle = partciclesArray[i].color;
+        ctx.lineWidth = 0.2;
+        ctx.moveTo(partciclesArray[i].x, partciclesArray[i].y);
+        ctx.lineTo(partciclesArray[j].x, partciclesArray[j].y);
+        ctx.stroke();
+        ctx.closePath();
+      }
+    }
+    if(partciclesArray[i].size <= 0.3) {
+      partciclesArray.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
+  requestAnimationFrame(animate);
+}
+animate();
+
+
+
+//Typewritter title
+class Typewritter {
+  constructor({ text, time = 300 }) {
+    this.texts = text.split(",");
+    this.sT = document.querySelector(".secondary__title");
+    this.realTime = time; 
+    this.wait = time + 1000; 
+    this.changeTime = 0;
+    this.length = this.texts.length;
+    this.countChar = 0; 
+    this.current = 0;
+    this.flag = false;
+    this.cutText();
+  }
+
+  cutText() {
+    const text = this.texts[this.current];
+    const tLength = this.texts[this.current].length;
+    
+    
+    if (this.countChar === tLength) {
+      this.changeTime = this.wait;
+    } 
+    
+    
+    if (this.flag && this.countChar === tLength - 1) {
+      this.changeTime = this.realTime / 4;
+    }
+      
+    
+    if (this.countChar === 0) {
+      this.changeTime = this.realTime;
+    }
+
+    this.drawText(text.substr(0, this.countChar));
+ 
+    if (!this.flag) {
+      this.countChar++;
+      if (this.countChar === tLength) {
+        this.flag = true;
+      }
+    } else if (this.flag) {
+      this.countChar--;
+      if (this.countChar === 0) {
+        this.flag = false;
+        this.current = ++this.current % this.length;
+      }
+    }
+
+    setTimeout(() => this.cutText(), this.changeTime);
+  }
+
+  drawText(chars) {
+    this.sT.innerHTML = chars;
+  }
+}
+
+new Typewritter({
+  text: "Lucile Gombert,développeuse web",
+  time: 100
+});
+
+
+
+
+
+
+
+
+
+
+
+
